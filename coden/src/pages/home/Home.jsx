a916c68db0fce15305082bac532bcbef59283da5
jEDI <ramkuntal2002@gmail.com>
2025-12-06 14:22:19 +0530

    Add files via upload

---
+++ coden/src/pages/home/Home.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.css";
+import axios from "axios";
+import { useLocation } from "react-router";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get("/posts" + search);
+        setPosts(res.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+    fetchPosts();
+  }, [search]);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
+    </>
+  );
+}
